6e5995a22ecffc2f42493f1427dda6dacafaace2
NayeemNishaat <rxnayeemimi@gmail.com>
2022-06-01 10:59:01 +0600

    updated AppError

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-const catchAsync =
+export const catchAsync =
 	(fn: Function) => (req: Request, res: Response, next: NextFunction) =>
 		fn(req, res, next).catch(next);
 
@@ -19,5 +19,3 @@ export class AppError extends Error {
 		Error.captureStackTrace(this, this.constructor);
 	}
 }
-
-export default catchAsync;
