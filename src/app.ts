0d438057dff01dc97f41a4ae52119e0c70d9a190
nayeemnishaat <rxnayeemimi@gmail.com>
2022-07-17 15:51:05 +0600

    updated structure

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 const app = express();
 import { AppError } from "./lib/error";
 import errorHandler from "./controllers/errorController";
+
+// Part: Enabling CORS
+app.use(cors());
 
 // Part: Parsing body
 app.use(express.json());
@@ -11,9 +15,7 @@ app.use(express.json());
 
 // Part: Handeling unwanted routes
 app.all("*", (req: Request, _res: Response, next: NextFunction) => {
-	next(
-		new AppError(`This (${req.originalUrl}) route is not available!`, 404)
-	);
+  next(new AppError(`This (${req.originalUrl}) route is not available!`, 404));
 });
 
 // Part: Handeling error
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,14 @@ const server = createServer(app);
 // Part: Configure DB Connection
 // TODO:
 
-const PORT: String | undefined = process.env.PORT || "5000";
+const PORT: String | undefined = process.env.APP_PORT || "5000";
 server.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}.`);
+  console.log(`Listening on port ${PORT}.`);
 });
+
+const gracefulShutdown = async () => {
+  process.exit();
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
